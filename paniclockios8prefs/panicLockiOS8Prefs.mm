1699f78849922bc30dc1bd33d2fd530b7a4a5337
Ruslan Ardashev <ardashev120101@gmail.com>
2014-12-23 20:31:54 -0500

    adds methods to prefs file

--- paniclockios8prefs/panicLockiOS8Prefs.mm
+++ paniclockios8prefs/panicLockiOS8Prefs.mm
@@ -11,6 +11,24 @@
 	}
 	return _specifiers;
 }
+
+-(void) sendEmail
+{
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"mailto:ruslan.ardashev@duke.edu?subject=panicLockPro"]];
+}
+
+-(void) openInstructions
+{
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://www.youtube.com/watch?v=0jrsolwbXxM"]];
+}
+
+-(void) openYoutubeChannel
+{
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://www.youtube.com/user/Ruslan120101"]];
+}
+
 @end
+
+
 
 // vim:ft=objc
