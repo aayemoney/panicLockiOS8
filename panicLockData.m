a1beb15183c6d921e115f7b45ec670aa3d12a83a
Ruslan Ardashev <ardashev120101@gmail.com>
2014-12-23 19:50:13 -0500

    adds primitive lock

--- panicLockData.m
+++ panicLockData.m
@@ -1,10 +1,22 @@
 @implementation panicLockData
 
 static BOOL isPanicLockActive;
+static NSLock *accessLock;
 
 + (BOOL)isPanicLockActive {
 
+	[panicLockData ensureLockAlloc];
 	return isPanicLockActive;
+
+}
+
++ (void)ensureLockAlloc {
+
+	if (accessLock == nil) {
+
+		accessLock = [[NSLock alloc] init];
+
+	}
 
 }
 
